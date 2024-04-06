c40be18222f3641bd8dd1c996305a303b79f5cd4
Kashyap Patel <kashoopatel80@gmail.comclear>
2024-04-06 09:35:33 +0530

    url update

--- backend/app.js
+++ backend/app.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://localhost:3001"], //included origin as true
+    origin: ["https://obligr.onrender.com"], //included origin as true
     credentials: true, //included credentials as true
   })
 );
@@ -24,9 +24,9 @@ app.use("/api/v1", user);
 
 app.use(express.static(path.join(__dirname, "../frontend/build")));
 
-// app.get("*", (req, res) => {
-//   res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
-// });
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
+});
 
 // Middleware for Errors
 app.use(errorMiddleware);
--- frontend/src/actions/userAction.js
+++ frontend/src/actions/userAction.js
@@ -49,7 +49,7 @@ export const login = (email, password) => async (dispatch) => {
     };
 
     const { data } = await axios.post(
-      `/api/v1/login`,
+      `https://obligrbe.onrender.com/api/v1/login`,
       { email, password },
       config
     );
@@ -65,7 +65,11 @@ export const register = (userData) => async (dispatch) => {
     dispatch({ type: REGISTER_USER_REQUEST });
 
     const config = { headers: { "Content-Type": "application/json" } };
-    const { data } = await axios.post(`/api/v1/register`, userData, config);
+    const { data } = await axios.post(
+      `https://obligrbe.onrender.com/api/v1/register`,
+      userData,
+      config
+    );
 
     dispatch({ type: REGISTER_USER_SUCCESS, payload: data.user });
   } catch (error) {
@@ -81,7 +85,7 @@ export const registeruseradmin = (userData) => async (dispatch) => {
     // dispatch({ type: REGISTER_USER_REQUEST });
     const config = { headers: { "Content-Type": "application/json" } };
     const { data } = await axios.post(
-      `http://localhost:3000/api/v1/registeruseradmin`,
+      `https://obligrbe.onrender.com/api/v1/registeruseradmin`,
       userData
     );
 
@@ -99,7 +103,7 @@ export const loadUser = () => async (dispatch) => {
   try {
     dispatch({ type: LOAD_USER_REQUEST });
 
-    const { data } = await axios.get(`http://localhost:3000/api/v1/me`);
+    const { data } = await axios.get(`https://obligrbe.onrender.com/api/v1/me`);
 
     dispatch({ type: LOAD_USER_SUCCESS, payload: data.user });
   } catch (error) {
@@ -110,7 +114,7 @@ export const loadUser = () => async (dispatch) => {
 // Logout User
 export const logout = () => async (dispatch) => {
   try {
-    await axios.get(`/api/v1/logout`);
+    await axios.get(`https://obligrbe.onrender.com/api/v1/logout`);
 
     dispatch({ type: LOGOUT_SUCCESS });
   } catch (error) {
@@ -125,7 +129,11 @@ export const updateProfile = (userData) => async (dispatch) => {
 
     const config = { headers: { "Content-Type": "multipart/form-data" } };
 
-    const { data } = await axios.put(`/api/v1/me/update`, userData, config);
+    const { data } = await axios.put(
+      `https://obligrbe.onrender.com/api/v1/me/update`,
+      userData,
+      config
+    );
 
     dispatch({ type: UPDATE_PROFILE_SUCCESS, payload: data.success });
   } catch (error) {
@@ -144,7 +152,7 @@ export const updatePassword = (passwords) => async (dispatch) => {
     const config = { headers: { "Content-Type": "application/json" } };
 
     const { data } = await axios.put(
-      `/api/v1/password/update`,
+      `https://obligrbe.onrender.com/api/v1/password/update`,
       passwords,
       config
     );
@@ -165,7 +173,11 @@ export const forgotPassword = (email) => async (dispatch) => {
 
     const config = { headers: { "Content-Type": "application/json" } };
 
-    const { data } = await axios.post(`/api/v1/password/forgot`, email, config);
+    const { data } = await axios.post(
+      `https://obligrbe.onrender.com/api/v1/password/forgot`,
+      email,
+      config
+    );
 
     dispatch({ type: FORGOT_PASSWORD_SUCCESS, payload: data.message });
   } catch (error) {
@@ -184,7 +196,7 @@ export const resetPassword = (token, passwords) => async (dispatch) => {
     const config = { headers: { "Content-Type": "application/json" } };
 
     const { data } = await axios.put(
-      `/api/v1/password/reset/${token}`,
+      `https://obligrbe.onrender.com/api/v1/password/reset/${token}`,
       passwords,
       config
     );
@@ -203,7 +215,9 @@ export const getAllUsers = () => async (dispatch) => {
   try {
     dispatch({ type: ALL_USERS_REQUEST });
 
-    const { data } = await axios.get(`/admin/users`);
+    const { data } = await axios.get(
+      `https://obligrbe.onrender.com/admin/users`
+    );
 
     dispatch({ type: ALL_USERS_SUCCESS, payload: data.users });
   } catch (error) {
@@ -215,7 +229,9 @@ export const getAllUsers = () => async (dispatch) => {
 export const getUserDetails = (id) => async (dispatch) => {
   try {
     dispatch({ type: USER_DETAILS_REQUEST });
-    const { data } = await axios.get(`/api/v1/admin/user/${id}`);
+    const { data } = await axios.get(
+      `https://obligrbe.onrender.com/api/v1/admin/user/${id}`
+    );
 
     dispatch({ type: USER_DETAILS_SUCCESS, payload: data.user });
   } catch (error) {
@@ -231,7 +247,7 @@ export const updateUser = (id, userData) => async (dispatch) => {
     const config = { headers: { "Content-Type": "application/json" } };
 
     const { data } = await axios.put(
-      `/api/v1/admin/user/${id}`,
+      `https://obligrbe.onrender.com/api/v1/admin/user/${id}`,
       userData,
       config
     );
@@ -250,7 +266,9 @@ export const deleteUser = (id) => async (dispatch) => {
   try {
     dispatch({ type: DELETE_USER_REQUEST });
 
-    const { data } = await axios.delete(`/api/v1/admin/user/${id}`);
+    const { data } = await axios.delete(
+      `https://obligrbe.onrender.com/api/v1/admin/user/${id}`
+    );
 
     dispatch({ type: DELETE_USER_SUCCESS, payload: data });
   } catch (error) {
